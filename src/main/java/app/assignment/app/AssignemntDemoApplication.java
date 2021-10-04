bf7515d40b467a9407085fbf0ad71b99b715de6a
ShujaWais <waisshuja99@gmail.com>
2021-10-05 02:34:26 +1100

    initial commit

---
+++ src/main/java/app/assignment/app/AssignemntDemoApplication.java
@@ -0,0 +1,13 @@
+package app.assignment.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignemntDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignemntDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/assignment/app/AssignemntDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package app.assignment.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignemntDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
